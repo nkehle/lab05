d9fcb619d31f0587e15b6d90f37186309b4776fd
Noa Kehle <nkehle@calpoly.edu>
2023-10-31 13:46:13 -0700

    upload

--- Disjoint.py
+++ Disjoint.py
@@ -1,6 +1,6 @@
 # Test git
 
-class DisjointSet:
+class Graph:
     def __init__(self):
         self.dict = {}  # initialize an empty dictionary
 
@@ -14,29 +14,12 @@ class DisjointSet:
         if v in self.dict and v == self.dict[v]:  # its label is itself
             return self.dict[v]
 
-    def union(self, u, v):
-        if u in self.dict and v in self.dict:
-            # find the labels for both
-            u_label = self.FindSet(u)
-            v_label = self.FindSet(v)
-
-            # turn all the keys with v into u labels
-            for key in self.dict:
-                if self.dict[key] == v_label:
-                    self.dict[key] = u_label
-
-            return u_label
+    def union(self, v, u):
+        for key in self.dict:
+            if self.dict[key] == v:
+                self.dict[key] = u
+        return u
 
 
-dis1 = DisjointSet()
-dis2 = DisjointSet()
-dis1.make_set('a')
-dis1.make_set('b')
-dis1.make_set('c')
-
-dis2.make_set('b')
-dis2.make_set('g')
-dis2.make_set('f')
-
-print(dis1.dict)
-print(dis2.dict)
+def Graph2DisjointSets(A):
+    return
