aeb9f98f7d7fab6f3f4dbcd591f54939ebd02cc5
Noa Kehle <nkehle@calpoly.edu>
2023-11-11 20:04:50 -0800

    working binheap with dictionary structure added

--- BinHeap.py
+++ BinHeap.py
@@ -3,8 +3,105 @@
 # CSC3-349-01 -- Fall 2023
 # Lab 5
 
-from heapq import heappush, heappop, heapify
+class BinHeap:
+    def __init__(self):
+        self.heap = []
+        self.nodes = {}
 
+    def init_list(self, v, s):
+        # set the values to infinite
+        self.heap = [(key, float('inf')) for key in v]
+        for i, (label, _) in enumerate(self.heap):
+            self.nodes[label] = {"index": i, "parent": -1}   # create the associative dict
+        self.decrease_key(s, 0)
+
+    def insert(self, x):
+        label, key = x
+        self.heap.append((label, float('inf')))
+        self.nodes[label] = {"index": len(self.heap) - 1, "parent": -1}
+        self.decrease_key(label, key)
+
+    def find_min(self):
+        return self.heap[0]
+
+    def delete_min(self):
+        if not self.heap:
+            return None
+        min_element = self.heap[0]
+        last_element = self.heap.pop()
+        if self.heap:
+            self.heap[0] = last_element
+            self.nodes[last_element[0]]["index"] = 0
+            self.heapify(0)
+        del self.nodes[min_element[0]]
+        return min_element
+
+    def decrease_key(self, label, new_key):
+        if label not in self.nodes:
+            return  # label not in the heap
+
+        index = self.nodes[label]["index"]
+        if new_key < self.heap[index][1]:
+            self.heap[index] = (label, new_key)
+            self._bubble_up(index)
+
+    def _bubble_up(self, i):
+        while i > 0:
+            parent = (i - 1) // 2
+            if self.heap[i][1] < self.heap[parent][1]:
+                self.swap(i, parent)
+                i = parent
+            else:
+                break
+
+    def heapify(self, i):
+        left_child = 2 * i + 1
+        right_child = 2 * i + 2
+        smallest = i
+
+        if left_child < len(self.heap) and self.heap[left_child][1] < self.heap[smallest][1]:
+            smallest = left_child
+
+        if right_child < len(self.heap) and self.heap[right_child][1] < self.heap[smallest][1]:
+            smallest = right_child
+
+        if smallest != i:
+            self.swap(i, smallest)
+            self.heapify(smallest)
+
+    def swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
+        self.nodes[self.heap[i][0]]["index"] = i
+        self.nodes[self.heap[j][0]]["index"] = j
+
+
+# Example usage:
+heap = BinHeap()
+heap.init_list(["1", "2", "3", "4", "5"], "3")
+print(heap.heap)
+heap.insert(("6", -100))
+print(heap.heap)
+
+
+
+"""pq.insert(("6", 2))
+print(pq.heap)
+print(pq.nodes)
+
+pq.decrease_key("1", 4)
+print(pq.heap)
+print(pq.nodes)
+
+print(pq.find_min())
+print(pq.delete_min())
+print(pq.heap)
+print(pq.nodes)"""
+
+
+"""from heapq import heappush, heappop, heapify
+
+
+structure for the tuple is (key, label)
 class BinHeap:
     # init the python list to structure the heap
     def __init__(self):
@@ -16,13 +113,16 @@ class BinHeap:
 
     # initialize the bin heap based on the list of v objects Ex -> [(1, 5), (2, 3), (3, 8), (4, 1)]
     def init_list(self, v, s):
-        self.heap = list(v)  # copy the list of tuples directly
-        self.heap[0] = (s[0], s[1]) # place s as the root
+        for tuple in v:
+            self.heap.append((tuple[1], tuple[0]))
+
+        self.heap[0] = (s[1], s[0]) # place s as the root
         heapify(self.heap)  # rearrange
 
     # insert an object v, ex [1, 2] vertex 1 with key 2
     def insert(self, v):
-        heappush(self.heap, v)
+        new_v = (v[1],v[0])
+        heappush(self.heap, new_v)
 
     # returns the object with the smallest key
     def find_min(self):
@@ -35,18 +135,19 @@ class BinHeap:
     # find the object with the given label and decrease its key to the new key
     def decrease_key(self, label, new_key):
         index = self.find_index(label)
-        self.heap[index][1] = new_key  # update the key
+        self.heap[index] = (label, new_key)  # update the key
         heapify(self.heap)  # rearrange
 
     # finds the index that has the matching target label
     def find_index(self, target):
-        for index, (current_key, current_label) in self.heap:
-            if current_label == target:
-                return index
+        for i in range(len(self.heap)):
+            if self.heap[i] == target:
+                return i
         # if not found
         return -1
 
-    def print_tree(self,):
+    # prints the tree with some spacing
+    def print_tree(self):
         height = (len(self.heap) - 1).bit_length()
         current_level = 0
         current_position = 0
@@ -59,7 +160,7 @@ class BinHeap:
             for _ in range(nodes_in_level):
                 if current_position < len(self.heap):
                     node = self.heap[current_position]
-                    print(' ' * spacing, f"({node[0]}, {node[1]})", end=" " * (mid - 1))
+                    print(' ' * spacing, f"({node[1]}, {node[0]})", end=" " * (mid - 1))
                     current_position += 1
                 else:
                     break
@@ -67,11 +168,14 @@ class BinHeap:
             current_level += 1
 
 
-
-v = [(1, 5), (2, 3), (3, 8), (4, 2), (5, 7)]
+v = [(1, 5), (2, 2), (3, 21)]
 que = BinHeap()
-que.init_list(v, (0,0))
-
-
-print(que.heap)
+que.init_list(v, (1, 5))
+print(que.heap, '\n')
 que.print_tree()
+
+que.insert((4, 0))
+print(que.heap, '\n')
+que.print_tree()
+"""
+
