1e4b0ea837794630123d40d4b66f39510110f812
Noa Kehle <nkehle@calpoly.edu>
2023-11-12 11:45:35 -0800

    kruskals fixed

--- Kruskals.py
+++ Kruskals.py
@@ -2,6 +2,8 @@
 # nkehle@calpoly.edu apokerlu@calpoly.edu
 # CSC3-349-01 -- Fall 2023
 # Lab 5
+import numpy as np
+
 import Disjoint
 def kruskals_mst(G):
     graph = Disjoint.Graph()
@@ -14,7 +16,7 @@ def kruskals_mst(G):
             if G[i, j] != 0:
                 edges.append((i, j, G[i, j]))
 
-    sorted(edges, key=lambda x: x[2])
+    edges = sorted(edges, key=lambda x: x[2])
 
     for i in range(len(G)):
         graph.make_set(i)
@@ -26,4 +28,13 @@ def kruskals_mst(G):
             A.append((graph.find_set(u), graph.find_set(v)))
             graph.union(u, v)
 
+    A = [edge for edge in A if None not in edge]    #remove the none edges
     return A
+
+
+A = np.array([[0, 2, 3],
+            [2, 0, 1],
+            [3, 1, 0]])
+
+print("Adjacency Matrix:\n", A)
+print("Kruskals: ", kruskals_mst(A))
